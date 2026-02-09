89f087509775c86fa478303f9e600722fce00e39
arjunvarikuppala <23eg112e63@anurag.edu.in>
2026-02-09 21:59:24 +0530

    remove env file from repository

---
+++ APIs/AuthorAPI.js
@@ -0,0 +1,115 @@
+import exp from "express";
+import { authenticate, register } from "../services/AuthService.js";
+import UserTypeModel from "../models/UserModel.js";
+
+import { checkAuthor } from "../middlewares/checkAuthor.js";
+import { verifyToken } from "../middlewares/verifyToken.js";
+import ArticleModel from "../models/ArticleModel.js";
+
+
+export const authorRoute = exp.Router();
+
+// Register author (public)
+authorRoute.post("/users", async (req, res) => {
+  try {
+    const userObj = req.body;
+    const newUserObj = await register({ ...userObj, role: "AUTHOR" });
+    res.status(201).json({ message: "author created", payload: newUserObj });
+  } catch (err) {
+    res.status(500).json({ message: "error creating author", error: err.message });
+  }
+});
+
+// Authenticate author (public)
+authorRoute.post("/authenticate", async (req, res) => {
+  try {
+    const userCred = req.body;
+    const { token, user } = await authenticate(userCred);
+
+    res.cookie("token", token, {
+      httpOnly: true,
+      sameSite: "lax",
+      secure: false,
+    });
+
+    res.status(200).json({ message: "login success", payload: user });
+  } catch (err) {
+    res.status(401).json({ message: "invalid credentials", error: err.message });
+  }
+});
+
+// Create article (protected)
+authorRoute.post(
+  "/articles",
+  verifyToken,
+  checkAuthor,
+  async (req, res) => {
+    try {
+      const article = req.body;
+      const newArticleDoc = new ArticleModel(article);
+      const createdArticleDoc = await newArticleDoc.save();
+
+      res
+        .status(201)
+        .json({ message: "article created", payload: createdArticleDoc });
+    } catch (err) {
+      res.status(500).json({ message: "error creating article", error: err.message });
+    }
+  }
+);
+
+// Read articles of author (protected)
+authorRoute.get(
+  "/articles/:authorId",
+  verifyToken,
+  checkAuthor,
+  async (req, res) => {
+    try {
+      const aid = req.params.authorId;
+
+      const articles = await ArticleModel.find({
+        author: aid,
+        isArticleActive: true,
+      }).populate("author", "firstName email");
+
+      res.status(200).json({ message: "articles", payload: articles });
+    } catch (err) {
+      res.status(500).json({ message: "error fetching articles", error: err.message });
+    }
+  }
+);
+
+// Edit article (protected)
+authorRoute.put(
+  "/articles",
+  verifyToken,
+  checkAuthor,
+  async (req, res) => {
+    try {
+      const { articleId, title, category, content, author } = req.body;
+
+      const articleOfDB = await ArticleModel.findOne({
+        _id: articleId,
+        author: author,
+      });
+
+      if (!articleOfDB) {
+        return res.status(404).json({ message: "Article not found" });
+      }
+
+      const updatedArticle = await ArticleModel.findByIdAndUpdate(
+        articleId,
+        { $set: { title, category, content } },
+        { new: true }
+      );
+
+      res
+        .status(200)
+        .json({ message: "article updated", payload: updatedArticle });
+    } catch (err) {
+      res.status(500).json({ message: "error updating article", error: err.message });
+    }
+  }
+);
+
+
---
+++ README.md
@@ -0,0 +1,20 @@
+### backend dev
+
+1.generate git repo
+    git init 
+2.add .gitignore file
+
+3.create .env file for environmenet variables and read data from .env with "dotenv" module // npm install dotenv
+
+4.genetare json pcked.json
+   npm init -y
+
+5.create express app
+
+6.connect to database
+
+7. Add the middlewares(body parser,err handling ,)
+ 
+8.design schema and create models
+
+9.design REST APIS fro all resources 
